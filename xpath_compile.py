9760e29397c463b5f5e7756481741462c5df0590
xiong jin <xiongjin402@gmail.com>
2019-06-11 17:15:27 +0800

    add requirements.txt

--- xpath_compile.py
+++ xpath_compile.py
@@ -49,148 +49,148 @@ xpath['dismiss_notification_offer'] = {
     "dismiss_elem_loc": "//button[text()='Not Now']"
 }
 
-xpath['extract_information'] = {
-    "close_overlay":"//div/div[@role='dialog']",
-    "one_pic_elem":["//section/main/article/div[1]/div/div[10]/div[",
-                    "3]/a/div"],
-    "like_element":"//a[@role='button']/span[text()='Like']/.."
-} #multiline:  one_pic_elem
-
-xpath['extract_post_info'] = {
-    "comment_list":"//div/ul",
-    "comments":"li",
-    "load_more_comments_element":"//div/ul/li[2]/button",
-    "load_more_comments_element_alt":"//div/ul/li[1]/button"
-}
-
-xpath['find_user_id'] = {
-    "meta_XP":"//meta[@property='instapp:owner_user_id']"
-}
-
-xpath['get_active_users'] = {
-    "latest_posts":"//div[contains(@class, '_9AhH0')]",
-    "likers_count":"//div[contains(@class,'Nm9Fw')]/child::button/span",
-    "likes_button":"//div[contains(@class,'Nm9Fw')]/child::button",
-    "next_button":  ["//a[contains(@class, 'HBoOv')]",
-                    "[text()='Next']"],
-    "topCount_elements":"//span[contains(@class,'g47SY')]"
-} #multiline:  next_button
-
-xpath['get_buttons_from_dialog'] = {
-    "follow_button":"//button[text()='Follow']",
-    "unfollow_button":"//button[text() = 'Following']"
-}
-
-xpath['get_comment_input'] = {
-    "comment_input": '//textarea[@placeholder = "Add a comment…"]',
-    "placeholder": '//input[@placeholder = "Add a comment…"]'
-}
-
-xpath['get_comments_on_post'] = {
-    "commenter_elem":"//h3/a",
-    "comments_block":"//div/div/h3/../../../..",
-    "like_button_full_XPath":"//div/span/button/span[@aria-label='Like']",
-    "unlike_button_full_XPath":"//div/span/button/span[@aria-label='Unlike']"
-}
-
-xpath['get_cord_location'] = {
-    "json_text":'//body'
-}
-
-xpath['get_following_status'] = {
-    "follow_button_XP":"//button[text()='Following' or \
-                                  text()='Requested' or \
-                                  text()='Follow' or \
-                                  text()='Follow Back' or \
-                                  text()='Unblock']"
-}
-
-xpath['get_follow_requests'] = {
-    "list_of_users":"//section/div",
-    "view_more_button":"//button[text()='View More']"
-}
-
-xpath['get_given_user_followers'] = {
-    "followers_link":'//a[@href="/{}/followers/"]'
-}
-
-xpath['get_given_user_following'] = {
-    "all_following":["//a[contains",
-                    "(@href,'following')]/span"],
-    "topCount_elements":"//span[contains(@class,'g47SY')]",
-    "following_link":'//a[@href="/{}/following/"]'
-} #multiline:  all_following
-
-xpath['get_photo_urls_from_profile'] = {
-    "photos_a_elems":"//div/a"
-}
-
-xpath['get_links_for_location'] = {
-    "top_elements": "//main/article/div[1]",
-    "main_elem": "//main/article/div[2]"
-}
-
-xpath['get_links_from_feed'] = {
-    "get_links": "//article/div[2]/div[2]/a"
-}
-
-xpath['get_links_for_tag'] = {
-    "top_elements": "//main/article/div[1]",
-    "main_elem": "//main/article/div[2]",
-    "possible_post": "//span[contains(@class, 'g47SY')]"
-}
-
-xpath['get_number_of_posts'] = {
-    "num_of_posts_txt":"//section/main/div/header/section/ul/li[1]/span/span",
-    "num_of_posts_txt_no_such_element":"//section/div[3]/div/header/section/ul/li[1]/span/span"
-}
-
-xpath['get_relationship_counts'] = {
-    "following_count":  ["//a[contains",
-                        "(@href,",
-                        "'following')]/span"],
-    "followers_count":  ["//a[contains",
-                        "(@href,",
-                        "'followers')]/span"],
-    "topCount_elements":"//span[contains(@class,'g47SY')]"
-} #multiline: following_count, followers_count
-
-xpath['get_source_link'] = {
-    "image":'//img[@class="FFVAD"]',
-    "image_alt": '//img[@class="_8jZFn"]',
-    "video":'//video[@class="tWeCl"]'
-}
-
-xpath['get_users_through_dialog'] = {
-    "find_dialog_box":"//body/div[3]/div/div[2]"
-}
-
-xpath['is_private_profile'] = {
-    "is_private":'//h2[@class="_kcrwx"]'
-}
-
-xpath['like_comment'] = {
-    "comments_block": "//div/div/h3/../../../..",
-    "span_like_elements": "//span[@aria-label='Like']",
-    "comment_like_button": ".."
-}
-
-xpath['like_image'] = {
-    "like": "//section/span/button/span[@aria-label='Like']",
-    "unlike": "//section/span/button/span[@aria-label='Unlike']"
-}
-
-xpath['like_from_image'] = {
-    "main_article": "//main//article//div//div[1]//div[1]//a[1]"
-}
-
-
-
-xpath['open_comment_section'] = {
-    "comment_elem":"//button/span[@aria-label='Comment']"
-}
-
-xpath['unfollow'] = {
-    "following_link":'//section//ul//li[3]',
-    "find_dialog_box":"//div[text()='Following']/../../../following-sibling::div"
-}
+# xpath['extract_information'] = {
+#     "close_overlay":"//div/div[@role='dialog']",
+#     "one_pic_elem":["//section/main/article/div[1]/div/div[10]/div[",
+#                     "3]/a/div"],
+#     "like_element":"//a[@role='button']/span[text()='Like']/.."
+# } #multiline:  one_pic_elem
+#
+# xpath['extract_post_info'] = {
+#     "comment_list":"//div/ul",
+#     "comments":"li",
+#     "load_more_comments_element":"//div/ul/li[2]/button",
+#     "load_more_comments_element_alt":"//div/ul/li[1]/button"
+# }
+#
+# xpath['find_user_id'] = {
+#     "meta_XP":"//meta[@property='instapp:owner_user_id']"
+# }
+#
+# xpath['get_active_users'] = {
+#     "latest_posts":"//div[contains(@class, '_9AhH0')]",
+#     "likers_count":"//div[contains(@class,'Nm9Fw')]/child::button/span",
+#     "likes_button":"//div[contains(@class,'Nm9Fw')]/child::button",
+#     "next_button":  ["//a[contains(@class, 'HBoOv')]",
+#                     "[text()='Next']"],
+#     "topCount_elements":"//span[contains(@class,'g47SY')]"
+# } #multiline:  next_button
+#
+# xpath['get_buttons_from_dialog'] = {
+#     "follow_button":"//button[text()='Follow']",
+#     "unfollow_button":"//button[text() = 'Following']"
+# }
+#
+# xpath['get_comment_input'] = {
+#     "comment_input": '//textarea[@placeholder = "Add a comment…"]',
+#     "placeholder": '//input[@placeholder = "Add a comment…"]'
+# }
+#
+# xpath['get_comments_on_post'] = {
+#     "commenter_elem":"//h3/a",
+#     "comments_block":"//div/div/h3/../../../..",
+#     "like_button_full_XPath":"//div/span/button/span[@aria-label='Like']",
+#     "unlike_button_full_XPath":"//div/span/button/span[@aria-label='Unlike']"
+# }
+#
+# xpath['get_cord_location'] = {
+#     "json_text":'//body'
+# }
+#
+# xpath['get_following_status'] = {
+#     "follow_button_XP":"//button[text()='Following' or \
+#                                   text()='Requested' or \
+#                                   text()='Follow' or \
+#                                   text()='Follow Back' or \
+#                                   text()='Unblock']"
+# }
+#
+# xpath['get_follow_requests'] = {
+#     "list_of_users":"//section/div",
+#     "view_more_button":"//button[text()='View More']"
+# }
+#
+# xpath['get_given_user_followers'] = {
+#     "followers_link":'//a[@href="/{}/followers/"]'
+# }
+#
+# xpath['get_given_user_following'] = {
+#     "all_following":["//a[contains",
+#                     "(@href,'following')]/span"],
+#     "topCount_elements":"//span[contains(@class,'g47SY')]",
+#     "following_link":'//a[@href="/{}/following/"]'
+# } #multiline:  all_following
+#
+# xpath['get_photo_urls_from_profile'] = {
+#     "photos_a_elems":"//div/a"
+# }
+#
+# xpath['get_links_for_location'] = {
+#     "top_elements": "//main/article/div[1]",
+#     "main_elem": "//main/article/div[2]"
+# }
+#
+# xpath['get_links_from_feed'] = {
+#     "get_links": "//article/div[2]/div[2]/a"
+# }
+#
+# xpath['get_links_for_tag'] = {
+#     "top_elements": "//main/article/div[1]",
+#     "main_elem": "//main/article/div[2]",
+#     "possible_post": "//span[contains(@class, 'g47SY')]"
+# }
+#
+# xpath['get_number_of_posts'] = {
+#     "num_of_posts_txt":"//section/main/div/header/section/ul/li[1]/span/span",
+#     "num_of_posts_txt_no_such_element":"//section/div[3]/div/header/section/ul/li[1]/span/span"
+# }
+#
+# xpath['get_relationship_counts'] = {
+#     "following_count":  ["//a[contains",
+#                         "(@href,",
+#                         "'following')]/span"],
+#     "followers_count":  ["//a[contains",
+#                         "(@href,",
+#                         "'followers')]/span"],
+#     "topCount_elements":"//span[contains(@class,'g47SY')]"
+# } #multiline: following_count, followers_count
+#
+# xpath['get_source_link'] = {
+#     "image":'//img[@class="FFVAD"]',
+#     "image_alt": '//img[@class="_8jZFn"]',
+#     "video":'//video[@class="tWeCl"]'
+# }
+#
+# xpath['get_users_through_dialog'] = {
+#     "find_dialog_box":"//body/div[3]/div/div[2]"
+# }
+#
+# xpath['is_private_profile'] = {
+#     "is_private":'//h2[@class="_kcrwx"]'
+# }
+#
+# xpath['like_comment'] = {
+#     "comments_block": "//div/div/h3/../../../..",
+#     "span_like_elements": "//span[@aria-label='Like']",
+#     "comment_like_button": ".."
+# }
+#
+# xpath['like_image'] = {
+#     "like": "//section/span/button/span[@aria-label='Like']",
+#     "unlike": "//section/span/button/span[@aria-label='Unlike']"
+# }
+#
+# xpath['like_from_image'] = {
+#     "main_article": "//main//article//div//div[1]//div[1]//a[1]"
+# }
+#
+#
+#
+# xpath['open_comment_section'] = {
+#     "comment_elem":"//button/span[@aria-label='Comment']"
+# }
+#
+# xpath['unfollow'] = {
+#     "following_link":'//section//ul//li[3]',
+#     "find_dialog_box":"//div[text()='Following']/../../../following-sibling::div"
+# }
